ea6d2645a101cade4fa5a4c45f2ccd98ee1e2b2c
Daniela1126 <mebahiah@gmail.com>
2024-09-11 20:11:59 +0000

    Aula Finalizada

--- Aula5j.java
+++ Aula5j.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 
 public class Aula5j {
 public static void main(String[]args){
+    //Atribuindo os valores iniciais
     Random aleatorio = new Random();
     Scanner leitura = new Scanner(System.in);
-    final int sorteio = aleatorio.nextInt(origin:1,bound:6);
+    final int sorteio = aleatorio.nextInt(6);
+
+    // Obtendo valores com o usuário
     System.out.println("Digite um numero");
-    int numero = leitura.nextInt();
-    if numero == sorteio) {
+    int numero = leitura.nextInt(), pontuacao = 30;
+
+    //Testes de pontuação
+    if( numero == sorteio) {
         System.out.println("Acertou");
         leitura.close();
         return;
@@ -16,15 +21,13 @@ public static void main(String[]args){
     System.out.println("Digite um numero");
     numero = leitura.nextInt();
     if (numero == sorteio) {
-        System.out.println("Acertou");    }
+        System.out.println("Você acertou com " + pontuacao + " pontos.");    
         leitura.close();
         return;
+    }else{
+        pontuacao = pontuacao -10;
+        System.out.println("Voce perdeu 10 pontos e está com " + pontuacao + " pontos");
     }
     leitura.close();
 }
-
-
-
-
-    
-
+}
