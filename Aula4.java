ba53bf9cc4223bd500b7d65fb8c64dfecf880051
Daniela1126 <mebahiah@gmail.com>
2024-09-16 00:38:17 +0000

    Corrigido

--- Aula4.java
+++ Aula4.java
@@ -1,24 +1,32 @@
-/**
- * Crie um jogo que o usuário digita um número e no final ele verifica se acertou o numero aleatorio
- */
 import java.util.Random;
 import java.util.Scanner;
+
 public class Aula4 {
     public static void main(String[] args) {
         Random aleatorio = new Random();
         Scanner lendoNumero = new Scanner(System.in);
-        final int sorteio = aleatorio.nextInt(0,10);
-        System.out.println("Digite um valor para jogar:");
-        int numeroDoUsuario = lendoNumero.nextInt();
-        System.out.println(numeroDoUsuario==sorteio);
-        System.out.println("Digite um valor para jogar:");
-        numeroDoUsuario = lendoNumero.nextInt();
-        System.out.println(numeroDoUsuario==sorteio);
-        System.out.println("Digite um valor para jogar:");
-        numeroDoUsuario = lendoNumero.nextInt();
-        System.out.println(numeroDoUsuario==sorteio);
-        System.out.println("Número aleatorio: " + sorteio +
-        " - Número digitado: " + numeroDoUsuario);
+        
+        // Gera um número aleatório entre 0 e 9
+        final int sorteio = aleatorio.nextInt(10);
+        int numeroDoUsuario;
+        
+        System.out.println("Tente adivinhar o número (entre 0 e 9):");
+        
+        // Loop para permitir várias tentativas
+        while (true) {
+            numeroDoUsuario = lendoNumero.nextInt();
+            
+            if (numeroDoUsuario == sorteio) {
+                System.out.println("Parabéns! Você acertou o número!");
+                break; // Sai do loop se o número estiver correto
+            } else {
+                System.out.println("Número incorreto. Tente novamente:");
+            }
+        }
+        
+        System.out.println("Número aleatório: " + sorteio +
+                           " - Número digitado: " + numeroDoUsuario);
+        
         lendoNumero.close();
     }
 }
