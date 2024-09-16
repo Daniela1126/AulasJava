559498fd08811c15950331b0d9a3f00981e50c5a
Daniela1126 <mebahiah@gmail.com>
2024-09-16 00:40:13 +0000

    Corrigido

--- Aula2.java
+++ Aula2.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
-import java.util.scanner;
-public class Aula2{
-    public static void main(String[] args){
+import java.util.Scanner; // Corrigido para usar a classe Scanner com "S" maiúsculo
+
+public class Aula2 {
+    public static void main(String[] args) {
         Scanner leitorTxt = new Scanner(System.in);
-        System.out.println("Digite seu nome:\n");
+        System.out.println("Digite seu nome:");
         String nome = leitorTxt.nextLine();
-        System.out.println("Seu nome é: " +nome);
+        System.out.println("Seu nome é: " + nome);
         leitorTxt.close();
     }
 }
