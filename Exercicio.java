0340cb24833889e0a29e92ee4fd9ee1f97b613af
Daniela1126 <mebahiah@gmail.com>
2024-09-16 00:26:39 +0000

    Corrigido

---
+++ Exercicio.java
@@ -0,0 +1,8 @@
+public class Exercicio {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int soma = a + b;
+        System.out.println("A soma de a e b é igual a " + soma);
+    }
+}
